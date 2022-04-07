5e55f6b7cd84bdda9b1aa6080fcdfe9bf218583b
Little Panda <>
2022-04-06 18:19:57 -0600

    Add test to return empty list when get trip from user that's not friend with currently logged in user

--- tripservice_mechanical/TripServiceTest.py
+++ tripservice_mechanical/TripServiceTest.py
@@ -2,28 +2,40 @@ import unittest
 from TripService import TripService
 from UserNotLoggedInException import UserNotLoggedInException
 from DependendClassCallDuringUnitTestException import DependendClassCallDuringUnitTestException
+from tripservice import User
+
 
 class TestableTripService(TripService):
-  def __init__(self, logged_state) -> None:
-      super().__init__()
-      self.logged_state = logged_state
+    def __init__(self, logged_state) -> None:
+        super().__init__()
+        self.logged_state = logged_state
 
-  def get_logged_user(self):
-      return self.logged_state
+    def get_logged_user(self):
+        return self.logged_state
+
 
 class TripServiceTest(unittest.TestCase):
 
-  def __init__(self, methodName: str = ...) -> None:
-      super().__init__(methodName)
-      self.UNLOGGED_USER = None
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName)
+        self.UNLOGGED_USER = None
+        self.LOGGED_USER = User()
+        self.ANOTHER_USER = User()
 
-  def test_should_throw_an_exception_when_the_user_is_not_logged_in(self):
-    trip_service = TestableTripService(self.UNLOGGED_USER)
+    def test_should_throw_an_exception_when_the_user_is_not_logged_in(self):
+        trip_service = TestableTripService(logged_state=self.UNLOGGED_USER)
 
-    with self.assertRaises(UserNotLoggedInException):
-      trip_service.getTripsByUser(self.UNLOGGED_USER)  
-  
-  
-    
-if __name__=="__main__":
-  unittest.main()
+        with self.assertRaises(UserNotLoggedInException):
+            trip_service.getTripsByUser(self.ANOTHER_USER)
+
+    def test_should_return_empty_trip_list_if_user_dont_have_friend(self):
+        trip_service = TestableTripService(logged_state=self.LOGGED_USER)
+
+        friend = User()
+        friend.addFriend(self.ANOTHER_USER)
+
+        self.assertEqual(len(trip_service.getTripsByUser(friend)), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
